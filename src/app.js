8ef9f72a8f9d565a363a73394e0d64d853417270
K-Mo2 <karimelsisi27@gmail.com>
2022-01-07 23:36:00 +0200

    adding the movie search logic

--- src/app.js
+++ src/app.js
@@ -21,4 +21,17 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.post("/search", async (req, res) => {
+  const { searchQuery } = req.body;
+
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`
+    );
+    res.send(response.data);
+  } catch (error) {
+    res.send(new Error(error));
+  }
+});
+
 module.exports = app;
